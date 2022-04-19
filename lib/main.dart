411e010a74f0597efb717abe9290f1276c711ff2
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-19 18:37:09 +0300

    added all the new files

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:wapi/screens/Home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
